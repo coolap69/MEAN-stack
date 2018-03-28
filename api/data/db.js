a2f5f05e5d63f711e02e2f36f12b8a9839d12907
Louis Pazos <coolap69@aol.com>
2018-03-28 18:05:13 +0000

    mongoose complex  schemas and models

--- api/data/db.js
+++ api/data/db.js
@@ -16,22 +16,25 @@ mongoose.connection.on('error', function(err) {
 });
 
 process.on('SIGINT', function() {
-   mongoose.connection.close(function() {
-       console.log('Mongoose disconnected throuh app termination (SIGINT)');
-       process.exit(0);
-   }); 
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected throuh app termination (SIGINT)');
+        process.exit(0);
+    });
 });
 
 process.on('SIGTERM', function() {
-   mongoose.connection.close(function() {
-       console.log('Mongoose disconnected throuh app termination (SIGTERM)');
-       process.exit(0);
-   }); 
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected throuh app termination (SIGTERM)');
+        process.exit(0);
+    });
 });
 
 process.once('SIGUSR2', function() {
-   mongoose.connection.close(function() {
-       console.log('Mongoose disconnected throuh app termination (SIGUSR2)');
-       process.kill(process.pid, 'SIGUSR2');
-   }); 
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected throuh app termination (SIGUSR2)');
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
+
+//BRING IN SCHEMAS AND MODELS
+require('./hotels.model.js');
