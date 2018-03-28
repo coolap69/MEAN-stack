d560d1721a5c8bf81b566908cbd794f6e49531bf
Louis Pazos <coolap69@aol.com>
2018-03-28 17:24:31 +0000

    mongoose installation and connections

--- api/data/db.js
+++ api/data/db.js
@@ -4,5 +4,34 @@ var dburl = 'mongodb://' + process.env.IP + ':27017/meanhotel';
 mongoose.connect(dburl);
 
 mongoose.connection.on('connected', function() {
-    console.log(Mongoose connected to ' + dburl');
+    console.log('Mongoose connected to ' + dburl);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error' + err);
+});
+
+process.on('SIGINT', function() {
+   mongoose.connection.close(function() {
+       console.log('Mongoose disconnected throuh app termination (SIGINT)');
+       process.exit(0);
+   }); 
+});
+
+process.on('SIGTERM', function() {
+   mongoose.connection.close(function() {
+       console.log('Mongoose disconnected throuh app termination (SIGTERM)');
+       process.exit(0);
+   }); 
+});
+
+process.once('SIGUSR2', function() {
+   mongoose.connection.close(function() {
+       console.log('Mongoose disconnected throuh app termination (SIGUSR2)');
+       process.kill(process.pid, 'SIGUSR2');
+   }); 
 });
